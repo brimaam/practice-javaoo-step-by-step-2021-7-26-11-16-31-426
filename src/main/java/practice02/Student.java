8dcc52ca81dec5796cc35641632d5517b599120a
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-04 16:10:38 +0800

    [should_person_have_name_and_age][AmeerBrima] feat: added the requirements for the person class

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,21 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+//    @Override
+//    public String introduce() {
+//        return "I am a " + + ". I am at Class " + this.klass +".";
+//    }
 }
