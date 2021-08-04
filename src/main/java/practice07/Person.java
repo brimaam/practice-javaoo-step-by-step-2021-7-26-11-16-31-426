7b5e074afd15a0e1be096461bed78a0d52fa35dc
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-04 19:47:36 +0800

    [should_person_have_id_name_and_age][AmeerBrima] feat: added the needed methods and attributes of person class

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,4 +1,30 @@
 package practice07;
 
 public class Person {
+    private int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
 }
