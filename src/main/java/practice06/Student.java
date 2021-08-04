fde429358dbef01f96024eb38a5566a94901eaba
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-04 19:16:59 +0800

    [should_student_have_name_age_and_class_number][AmeerBrima] feat: added student class attributes and methods

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,18 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    private Klass klassObject;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klassObject = klass;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klassObject;
+    }
 }
