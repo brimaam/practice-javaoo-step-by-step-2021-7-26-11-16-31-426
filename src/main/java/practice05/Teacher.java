fb6909c2c928249d6b05a2facb39eeeb8ebbbcac
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-04 18:09:48 +0800

    [should_teacher_have_name_and_age_and_class][AmeerBrima] feat: added the needed attributes and inherited attributes and methods of teacher class

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,18 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
