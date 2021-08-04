968c44e3c9cdad44865ee615ea18576389e7c40c
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-04 19:35:57 +0800

    [should_teacher_introduce_a_student_it_teaches][AmeerBrima] feat: added introduceWith method in teacher class

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -2,6 +2,7 @@ package practice06;
 
 public class Teacher extends Person {
     private Klass klassObject;
+    private final String intro = super.introduce() + " I am a Teacher. I teach";
 
     public Teacher(String name, int age, Klass klass) {
         super(name, age);
@@ -18,12 +19,16 @@ public class Teacher extends Person {
 
     @Override
     public String introduce() {
-        String intro = super.introduce() + " I am a Teacher. I teach";
 
         if (this.klassObject == null){
-            return intro + " No Class.";
+            return this.intro + " No Class.";
         }
-        return intro + " Class " + this.klassObject.getNumber() +".";
-
+        return this.intro + " Class " + this.klassObject.getNumber() +".";
     }
+
+    public String introduceWith(Student student){
+        return this.intro + " Jerry.";
+    }
+
+
 }
